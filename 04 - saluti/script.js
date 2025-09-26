aa1f3faf7c86a779f5695f86e60ef472d18fc6a4
cristianmoletto <cristian.moletto@gmail.com>
2025-09-26 16:48:23 +0200

    feat: 4° esercizio completato

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,10 +4,23 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+/***
+ * @param {string} name;
+ * @returns {string} 
+ */
+function salutaNome(name){
+    return `Ciao ${name}`
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const result = salutaNome(name);
+console.log(result)
 
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+
+
+// Versione breve
+const saluta = (name) => `Ciao ${name}`;
+console.log(result);
