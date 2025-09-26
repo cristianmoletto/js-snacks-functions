1c78f8adcb773d2412eed96003305f2f7aeba917
cristianmoletto <cristian.moletto@gmail.com>
2025-09-26 16:42:50 +0200

    feat: esercizio 3 completato

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,26 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+/***
+ * @param {string} word
+ * @returns {number}
+ */
+function contaVocali(word) {
+    let array = ["a","e","i","o","u"]; 
+    let vocali = 0;
+
+    for (i = 0; i < word.length; i++) { 
+        if (array.includes(word[i])){
+            vocali++;
+        }
+    }
+    return vocali;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const results = contaVocali(word)
+console.log(results)
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
