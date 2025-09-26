5918563304655d82bf4e704ef35d32b14394e1a6
cristianmoletto <cristian.moletto@gmail.com>
2025-09-26 16:31:11 +0200

    feat: secondo esercizio svolto

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,26 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+/***
+ * @param {array} array
+ * @param {string} letter
+ * @returns {array}
+ */
+function filterByLetter(array, letter) {
+    let risultati = [];
+    for (i = 0; i < array.length; i++) {
+        if (array[i].includes(letter)) {
+            risultati.push(array[i])
+        }
+    }
+    return risultati;
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let results = filterByLetter(names, "A")
+console.log(results)
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
