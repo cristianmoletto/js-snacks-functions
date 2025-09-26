5fc8af5f876fd2d989c5ab256aaa6e365308190c
cristianmoletto <cristian.moletto@gmail.com>
2025-09-26 16:57:45 +0200

    feat: ultimo esercizio svolto

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -7,12 +7,31 @@ e buonasera se è sera (oltre le 17)
 
 const name = 'Mario';
 
-
 // Dichiara la funzione qui.
+/**
+ * @param {string} name;
+ * @returns {string}
+ */
+function salutoPersonalizzato(name) {
+    const today = new Date
+    const hour = today.getHours()
 
+    let message = ""
+
+    if (hour <= 5 && hour < 13) {
+        message = "Buongiorno " + name;
+    } else if (hour >= 13 && hour < 17) 
+        { message = "Buon pomeriggio " + name; } 
+    else 
+    { message = "Buonasera " + name; }
+
+    return message;
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const saluto = salutoPersonalizzato(name);
+console.log(saluto);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
